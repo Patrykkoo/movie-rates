1de27b83cce1afae2a194e1d058a2803946c8554
Patrykkoo <patryk.chojnacki02@gmail.com>
2025-06-22 17:43:09 +0200

    Konfuguracja sesji w server.js

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const path = require('path');
+const session = require('express-session');
 
+// Importujemy routery
 const movieRoutes = require('./routes/movieRoutes');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,7 +16,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(session({
+    secret: 'bardzo-tajny-klucz-sesji',
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use((req, res, next) => {
+    res.locals.isAuthenticated = !!req.session.userId;
+    res.locals.username = req.session.username;
+    next();
+});
+
 app.use('/', movieRoutes);
+app.use('/', authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Serwer Movierates działa na porcie ${PORT}`);
